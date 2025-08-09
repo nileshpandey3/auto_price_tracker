6db4d61a383a5ea84fc4f3869c52e7cbb7342c33
Nilesh Pandey <nileshpandey3@gmail.com>
2025-08-09 14:20:32 -0700

    add 1 simple test

--- tests/fetcher_test.py
+++ tests/fetcher_test.py
@@ -1,8 +1,13 @@
-import pytest
+import pytest, ipdb
+
+from tracker.fetcher import fetch_url
 
 @pytest.mark.fetcher
 class TestFetcher:
 
     @pytest.mark.validate_url
     def test_validate_url(self):
-        pass
+        url = 'https://example.com'
+        resp = fetch_url(url)
+        assert isinstance(resp, str)
+        
--- tracker/fetcher.py
+++ tracker/fetcher.py
@@ -2,7 +2,6 @@ import requests
 import ipdb
 
 def fetch_url(url):
-    ipdb.set_trace()
     resp = requests.get(url)
     resp.raise_for_status()
     return resp.text
